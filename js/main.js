36e966e604d8447e4bc9fa946a909a5aa73e372e
Nuccia955 <elenamunaro1995@gmail.com>
2021-11-15 15:57:26 +0100

    add Milestone 1

--- js/main.js
+++ js/main.js
@@ -2,6 +2,19 @@
 const app = new Vue ({
     el: '#app',
     data: {
-        
+        todos: [
+            {
+                text: 'fare la spesa',
+                completed: true,
+            },
+            {
+                text: 'pulire casa',
+                completed: false,
+            },
+            {
+                text: 'preparare la cena',
+                completed: true,
+            },
+        ],
     }
 })
